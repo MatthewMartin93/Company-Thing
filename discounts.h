306ede51b4575ba5041c4dbd0bd607aad52b2b8f
MatthewMartin93 <161871982+MatthewMartin93@users.noreply.github.com>
2025-05-20 08:24:44 -0400

    Update discounts.h

--- discounts.h
+++ discounts.h
@@ -1,1 +1,20 @@
+#pragma once
+#include <string>
 
+void shane(int &x, std::string y) {
+    if (y == "SHANE") {
+        x = 10;
+    }
+}
+
+void henry(int &x, std::string y) {
+    if (y == "HENRY") {
+        x = 20;
+    }
+}
+
+void pricken(int &x, std::string y) {
+    if (y == "PRICKEN") {
+        x = 25;
+    }
+}
